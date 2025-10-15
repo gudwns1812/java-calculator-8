d8dbe3829b3682ec033b35c13206f2d8e10deb55
gudwns1812 <gudwns1812@naver.com>
2025-10-15 15:45:37 +0900

    chore: shutdownHook 등록

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,6 +3,7 @@ package calculator;
 import calculator.controller.CalculatorController;
 import calculator.model.Calculator;
 import calculator.service.CalculatorService;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
@@ -10,6 +11,10 @@ public class Application {
         Calculator calculator = new Calculator();
         CalculatorService service = new CalculatorService(calculator);
         CalculatorController controller = new CalculatorController(service);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(Console::close, "shutdownHook"));
+
         controller.run();
+
     }
 }
