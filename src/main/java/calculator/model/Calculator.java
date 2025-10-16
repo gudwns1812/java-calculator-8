97c245b8cbce377cc26d47d9366f52be03c0a6c3
gudwns1812 <gudwns1812@naver.com>
2025-10-16 15:40:41 +0900

    feat: 음수 예외 처리 기능 추가

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -7,7 +7,14 @@ public class Calculator {
     public int add(String[] numbers) {
         return Arrays.stream(numbers)
                 .map(Integer::parseInt)
+                .peek(Calculator::validateNegative)
                 .reduce(Integer::sum)
                 .orElseThrow(() -> new IllegalArgumentException("올바른 값이 아닙니다."));
+    }
+
+    private static void validateNegative(Integer i) {
+        if (i < 0) {
+            throw new IllegalArgumentException("음수는 허용되지 않습니다.");
+        }
     }
 }
--- src/test/java/calculator/service/CalculatorServiceTest.java
+++ src/test/java/calculator/service/CalculatorServiceTest.java
@@ -1,6 +1,7 @@
 package calculator.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import calculator.model.Calculator;
 import calculator.model.DelimiterManager;
@@ -10,20 +11,18 @@ import org.junit.jupiter.api.Test;
 
 class CalculatorServiceTest {
 
-    private Calculator calculator;
-    private DelimiterManager manager;
     private CalculatorService service;
 
     @BeforeEach
     void setUp() {
-        calculator = new Calculator();
-        manager = new DelimiterManager();
+        Calculator calculator = new Calculator();
+        DelimiterManager manager = new DelimiterManager();
         service = new CalculatorService(calculator, manager);
     }
 
     @Test
     @DisplayName("빈 문자열을 입력하면 0이 나와야한다.")
-    public void empty_expression() {
+    void empty_expression() {
         //given
         String testInput = "";
         //when
@@ -34,7 +33,7 @@ class CalculatorServiceTest {
 
     @Test
     @DisplayName("기본 구분자를 이용해 계산한다.")
-    public void basic_delimiter() {
+    void basic_delimiter() {
         //given
         String input = "1,2:3";
         //when
@@ -45,7 +44,7 @@ class CalculatorServiceTest {
 
     @Test
     @DisplayName("커스텀 구분자를 이용해 숫자를 계산한다.")
-    public void using_custom_delimiter() {
+    void using_custom_delimiter() {
         //given
         String input = "//;\\n1;2;3";
         //when
@@ -53,4 +52,40 @@ class CalculatorServiceTest {
         //then
         assertThat(result).isEqualTo(6);
     }
+
+    @Test
+    @DisplayName("커스텀 구분자와 기본 구분자를 혼합해 사용한다.")
+    void custom_and_basic_delimiter() {
+        //given
+        String input = "//;\\n1;2,3:4";
+        //when
+        int result = service.splitExpression(input);
+        //then
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자가 한자리가 아닌 경우 테스트")
+    void multi_digits_delimiter() {
+        //given
+        String input = "//!!!;;\\n1!!!;;2!!!;;3";
+        //when
+        int result = service.splitExpression(input);
+        //then
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자를 이용해 음수 예외 처리를 검증한다.")
+    void using_custom_delimiter_negative_number() {
+        //given
+        String input = "//;\\n-1;2;3";
+        //when
+
+        //then
+        assertThatThrownBy(() -> service.splitExpression(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
