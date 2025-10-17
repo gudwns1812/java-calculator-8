9dac6dbdba21ab4990d40e81a7bfb1b249dd6be7
gudwns1812 <gudwns1812@naver.com>
2025-10-17 11:36:36 +0900

    refactor: View 메세지를 상수로 변환

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -3,9 +3,10 @@ package calculator.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
 
     public static String readLine() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        System.out.println(INPUT_MESSAGE);
         return Console.readLine();
     }
 }
