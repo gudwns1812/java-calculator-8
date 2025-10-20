bb667a5e84f1fd2dd8cec46e1258fd4f0a4df338
gudwns1812 <gudwns1812@naver.com>
2025-10-20 17:28:56 +0900

    refactor($CalculatorTest): CalculatorTest 단위 테스트 분리

--- src/test/java/calculator/model/CalculatorTest.java
+++ src/test/java/calculator/model/CalculatorTest.java
@@ -3,68 +3,62 @@ package calculator.model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import calculator.service.CalculatorService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
-    private DelimiterManager manager;
-    private CalculatorService service;
+    private Calculator calculator;
 
     @BeforeEach
     void setUp() {
-        Calculator calculator = new Calculator(new NumberValidator(), new NumberParser());
-        manager = new DelimiterManager();
-        service = new CalculatorService(calculator, manager);
+        calculator = new Calculator(new NumberValidator(), new NumberParser());
     }
 
     @Test
     @DisplayName("빈 문자열을 입력하면 0이 나와야한다.")
     void empty_expression() {
         //given
-        String testInput = "";
+        String[] numbers = {""};
         //when
-        long result = service.addNumbers(testInput);
+        long result = calculator.add(numbers);
         //then
         assertThat(result).isEqualTo(0L);
     }
 
 
     @Test
-    @DisplayName("커스텀 구분자를 이용해 음수 예외 처리를 검증한다.")
+    @DisplayName("음수 예외 처리를 검증한다.")
     void using_custom_delimiter_negative_number() {
         //given
-        String input = "-1;2;3";
-        manager.addCustomRegex(";");
+        String[] numbers = {"-1", "2", "3"};
         //when
 
         //then
-        assertThatThrownBy(() -> service.addNumbers(input))
+        assertThatThrownBy(() -> calculator.add(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    @DisplayName("커스텀 구분자를 이용해 문자 예외 처리를 검증한다.")
+    @DisplayName("숫자 문자열에 문자가 들어올 경우 예외 처리를 검증한다.")
     void check_not_number_test() {
         //given
-        String input = "a;b;c";
-        manager.addCustomRegex(";");
+        String[] numbers = {"a", "2", "3"};
         //when
 
         //then
-        assertThatThrownBy(() -> service.addNumbers(input))
+        assertThatThrownBy(() -> calculator.add(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    @DisplayName("구분자가 연속 2번 나올경우 0으로 처리한다.")
+    @DisplayName("숫자 사이에 빈 문자열을 0으로 처리한다.")
     void in_a_row_delimiter() {
         //given
-        String input = "1,,2:3";
+        String[] numbers = {"1", "", "2", "3"};
         //when
-        long result = service.addNumbers(input);
+        long result = calculator.add(numbers);
         //then
         assertThat(result).isEqualTo(6);
     }
